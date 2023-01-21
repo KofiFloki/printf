2030d47b9d2a518849dc156195b95516c65a162b
OmobaVII <omoba.career@gmail.com>
2023-01-21 13:04:49 -0800

    changed the structure of print_string

--- test_files/print_string.c
+++ test_files/print_string.c
@@ -3,7 +3,8 @@
 int print_string(char *string){
   int count = 0;
 
-  for (int i = 0; string[i] != '\0'; i++){
+  for (int i = 0; string[i] != '\0'; i++)
+  {
     count += _putchar(string[i]);
   }
   
